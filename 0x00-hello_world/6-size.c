74bf94ed9e3192da2694c751037b0d1c0dee600d
Salah-EddineArsalane <arsalane.salaheddine@gmail.com>
2024-06-24 04:34:21 -0700

    Test 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,9 +2,14 @@
 /**
  * main - Entery point
  *
- * Return : Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
-{ printf("Size of a char: %zu byte(s)\nSize of an int: %zu byte(s)\nSize of a long int: %zu byte(s)\nSize of a long long int: %zu byte(s)\nSize of a float: %zu byte(s)\n", sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float));
+{ printf("Size of a char: %zu byte(s)\nSize of an int: %zu byte(s)\nSize of a long int: %zu byte(s)\nSize of a long long int: %zu byte(s)\nSize of a float: %zu byte(s)\n",
+sizeof(char),
+sizeof(int),
+sizeof(long int),
+sizeof(long long int),
+sizeof(float));
 return (0);
 }
