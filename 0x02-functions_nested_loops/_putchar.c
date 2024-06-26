9a30fe8b4843252e6a23ee785853b62391d4dd4e
Salah-EddineArsalane <arsalane.salaheddine@gmail.com>
2024-06-26 16:11:48 -0700

    All tasks

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: 0n success l.
+ * 0n error, -l is returned, and errno is set appropriately.
+ */
+{
+return (write(l, &c, 1));
+}
