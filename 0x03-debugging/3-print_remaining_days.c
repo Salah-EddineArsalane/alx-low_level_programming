5e0d6e604c5efe967eb83cf434be32aa269452a3
Salah-EddineArsalane <arsalane.salaheddine@gmail.com>
2024-06-28 12:24:21 -0700

    test

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,56 +12,26 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-    int leap = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+{
+if (month > 2 || (month == 2 && day == 29))
+{
+day++;
+}
 
-    if (leap && month >= 2) {
-        day++;
-    }
-
-    switch (month) {
-        case 1:
-            break;
-        case 2:
-            day += 31;
-            break;
-        case 3:
-            day += leap ? 60 : 59;
-            break;
-        case 4:
-            day += leap ? 91 : 90;
-            break;
-        case 5:
-            day += leap ? 121 : 120;
-            break;
-        case 6:
-            day += leap ? 152 : 151;
-            break;
-        case 7:
-            day += leap ? 182 : 181;
-            break;
-        case 8:
-            day += leap ? 213 : 212;
-            break;
-        case 9:
-            day += leap ? 244 : 243;
-            break;
-        case 10:
-            day += leap ? 274 : 273;
-            break;
-        case 11:
-            day += leap ? 305 : 304;
-            break;
-        case 12:
-            day += leap ? 335 : 334;
-            break;
-        default:
-            break;
-    }
-
-    if (leap && month == 2 && day > 60) {
-        printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-    } else {
-        printf("Day of the year: %d\n", day);
-        printf("Remaining days: %d\n", leap ? 366 - day : 365 - day);
-    }
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 366 - day);
+}
+else
+{
+if (month == 2 && day == 60)
+{
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+}
+else
+{
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 365 - day);
+}
+}
 }
