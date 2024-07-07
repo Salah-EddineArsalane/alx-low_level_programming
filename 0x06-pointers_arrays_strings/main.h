38842d74d113b8827522b7731f84dc6166c66753
Salah-EddineArsalane <arsalane.salaheddine@gmail.com>
2024-07-07 15:41:24 -0700

    Task 8

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 char *_strcat(char *dest, char *src);
 char *string_toupper(char *str);
 char *cap_string(char *str);
+char *leet(char *str);
 void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
 int _putchar(char c);
