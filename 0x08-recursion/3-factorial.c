ae8778413b5b1869d64ac65923bd9f29dd1b3d06
Salah-EddineArsalane <arsalane.salaheddine@gmail.com>
2024-07-10 06:20:37 -0700

    Betty Test

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,7 +4,7 @@
  * factorial - returns the factorial of a given number
  * @n: the number to calculate the factorial of
  * 
- * Return: the factorial of n, or -1 if n is less than 0
+ * Return: 0
  */
 int factorial(int n)
 {
