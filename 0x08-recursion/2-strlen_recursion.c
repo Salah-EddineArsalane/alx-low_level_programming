80e30760f235c41a0f10eeb562b58a09dca9d5f8
Salah-EddineArsalane <arsalane.salaheddine@gmail.com>
2024-07-10 06:09:42 -0700

    Betty Test

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,7 +4,7 @@
  * _strlen_recursion - returns the length of a string
  * @s: the string to measure
  * 
- * Return: the length of the string
+ * Return:the length of the string
  */
 int _strlen_recursion(char *s)
 {
