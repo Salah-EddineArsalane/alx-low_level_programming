d3e04c7e92cd41b4d658dbcbd20be448af75aad3
Salah-EddineArsalane <arsalane.salaheddine@gmail.com>
2024-07-22 16:48:47 -0700

    3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+
 /**
  * struct dog - a dog's basic info
  * @name: First member
@@ -24,5 +26,11 @@ char *owner;
  * @owner: owner to initialize
  */
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/**
+ * print_dog - prints a struct dog
+ * @d: pointer to struct dog to print
+ */
+void print_dog(struct dog *d);
 
 #endif
